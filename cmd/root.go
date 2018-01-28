7185e4563f9c59e8442360d2031d0c4cf027cdaf
Milutin Jovanović <jovanovic.milutin@gmail.com>
2018-01-28 15:59:59 -0500

    updated copyright email

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Milutin Jovanović miki@voreni.com
+// Copyright © 2018 Milutin Jovanović jovanovic.milutin@gmail.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
